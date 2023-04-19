52f0da621e90991a9b1010c10fc7a6cea516b067
Nicolas Leon <nicolasleon@nicolasc3mxq05n.myfiosgateway.com>
2023-04-19 01:00:33 -0400

    directory name fix?

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 const app = express();
 const port = 3001;
 
-
+app.use(express.static(__dirname + '/public'));
 app.get("/", function (req, res) {
 
     var data = JSON.parse(fs.readFileSync('timestamp.json', 'utf8'))
